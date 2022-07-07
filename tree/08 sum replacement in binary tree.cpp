d24554bc056e187b27a8077c8b3dc1e52725d264
Ashutosh Sharma <90125147+Mr-Sharma01@users.noreply.github.com>
2022-07-07 22:45:37 +0530

    Create 08 sum replacement in binary tree.cpp

---
+++ tree/08 sum replacement in binary tree.cpp
@@ -0,0 +1,66 @@
+
+//Sum Replacement in Binary Tree
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left, *right;
+    
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void view(struct Node* node) {
+  if (node == NULL)
+    return;
+
+  cout << node->data << "->";
+  view(node->left);
+  view(node->right);
+}
+
+void sum(Node* node)
+    {
+        if(node == NULL)
+        {
+            return ;
+        }
+        
+         sum(node->left);
+         sum(node->right);
+        
+        if (node->left != NULL)
+        {
+            node->data = node->data + node->left->data;
+        }
+        if (node->right != NULL)
+        {
+            node->data = node->data + node->right->data;
+        }
+        
+    }
+
+int main(){
+    Node* root = new Node (1) ;
+    root->left = new Node (2) ;
+    root->right = new Node (3) ;
+  
+    root->left->left = new Node (4) ;
+    root->left->right = new Node (5) ;
+    root->right->left = new Node (6) ;
+    root->right->right = new Node (7) ;
+    
+    view(root);
+    cout<<endl;
+    sum(root);
+    view(root);
+    
+  return 0;
+}
