8451c5c8fd62adcfc176cbf2502455b9c43695ba
Ashutosh Sharma <90125147+Mr-Sharma01@users.noreply.github.com>
2022-07-09 17:01:20 +0530

    Create 011 Lowest common ancestor.cpp

---
+++ tree/011 Lowest common ancestor.cpp
@@ -0,0 +1,43 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left, *right;
+    
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+struct Node* lca(Node* node, int n1, int n2)
+{
+    if(node==NULL)return NULL;
+    if(node->data==n1 || node->data==n2)return node;
+    
+    Node* left = lca(node->left,n1,n2);
+    Node* right = lca(node->right,n1,n2);
+    
+    if(left==NULL)return right;
+    if(right==NULL)return left;
+    
+    return node;
+}
+
+int main(){
+    Node* root = new Node (1) ;
+    root->left = new Node (2) ;
+    root->right = new Node (3) ;
+  
+    root->left->left = new Node (4) ;
+    root->left->right = new Node (5) ;
+    root->right->left = new Node (6) ;
+    root->right->right = new Node (7) ;
+  
+    cout<<lca(root,3,7)->data;
+    
+  return 0;
+}
