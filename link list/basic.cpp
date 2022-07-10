0682ba1c6f77bc80e05407ed3f504227280385cf
Ashutosh Sharma <90125147+Mr-Sharma01@users.noreply.github.com>
2022-07-10 22:08:52 +0530

    Create basic.cpp

---
+++ link list/basic.cpp
@@ -0,0 +1,34 @@
+
+// basic linked list program
+
+
+#include <bits/stdc++.h>
+using namespace std;
+  
+class Node {
+public:
+    int data;
+    Node* next;
+};
+  
+int main()
+{
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+  
+    head = new Node();
+    second = new Node();
+    third = new Node();
+  
+    head->data = 1; 
+    head->next = second; 
+  
+    second->data = 2;
+    second->next = third;
+  
+    third->data = 3; // assign data to third node
+    third->next = NULL;
+  
+    return 0;
+}
