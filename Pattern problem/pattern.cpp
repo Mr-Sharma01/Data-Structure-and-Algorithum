c5ff50f7e58b3310d9e0c180238193f4eebe22e6
Ashutosh Sharma <90125147+Mr-Sharma01@users.noreply.github.com>
2023-04-28 11:31:11 +0530

    Update pattern.cpp

--- Pattern problem/pattern.cpp
+++ Pattern problem/pattern.cpp
@@ -178,3 +178,98 @@ int main()
 
 
 #7
+Input Format: N = 6
+Result:
+     *     
+    ***    
+   *****   
+  *******  
+ ********* 
+***********
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 6;
+    for(int i =0; i<n; i++)
+    {  for(int j=n; j>i; j--)
+        {
+        cout<<" ";
+        }
+        for(int k=0; k<2*i+1; k++)
+        {
+            cout<<"*";
+        }
+    cout<<endl;
+    }    
+    return 0;
+}
+
+#8
+Input Format: N = 6
+Result:     
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
+
+#include <iostream>                 //My-way
+
+using namespace std;
+
+int main()
+{
+    int n = 6;
+    for(int i =0; i<n; i++)
+    {  for(int j=0; j<i; j++)
+        {
+        cout<<" ";
+        }
+        for(int k=n*2-1; k>=2*i+1; k--)
+        {
+            cout<<"*";
+        }
+    cout<<endl;
+    }    
+    return 0;
+}
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern8(int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+       
+        for(int j=0;j< 2*N -(2*i +1);j++){
+            
+            cout<<"*";
+        }
+        
+         for (int j =0; j<i; j++)
+        {
+            cout <<" ";
+        }
+        
+        cout << endl;
+    }
+}
+
+int main()
+{   
+    int N = 5;
+
+    pattern8(N);
+
+    return 0;
+}
